f4505a94c8d9ed883e06839815f4ea5248b23aeb
BoWen Chai <lovebaihezi@gmail.com>
2025-08-20 20:52:25 +0800

    fixup: basic navbar

--- playwright.config.ts
+++ playwright.config.ts
@@ -2,8 +2,8 @@ import { defineConfig } from '@playwright/test';
 
 export default defineConfig({
 	webServer: {
-		command: 'npm run build && npm run preview',
-		port: 4173
+		command: 'pnpm preview',
+		port: 8787
 	},
 	testDir: 'e2e'
 });
--- src/routes/page.svelte.spec.ts
+++ src/routes/page.svelte.spec.ts
@@ -1,10 +1,12 @@
-import { page } from '@vitest/browser/context';
 import { describe, expect, it } from 'vitest';
 import { render } from 'vitest-browser-svelte';
 import Page from './+page.svelte';
 
 describe('/+page.svelte', () => {
 	it('Render one navbar displaying icon and Team Roller', async () => {
-		render(Page);
+		const { getByAltText, getByText } = render(Page);
+
+		expect(getByAltText('LqxcLqxc Logo')).toBeInTheDocument();
+		expect(getByText('Team Roller')).toBeInTheDocument();
 	});
 });
